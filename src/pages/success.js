05a677ed07e10b89c8b15750653b46f9b867dbcc
lcsrbn <luca.sorbini@gmail.com>
2020-02-26 21:05:46 +0100

    initial commit

--- src/pages/success.js
+++ src/pages/success.js
@@ -1,24 +1,33 @@
-import React from 'react';
-import Layout from '../components/layout';
+import React from "react";
+import Layout from "../components/layout";
+import SEO from "../components/seo";
+import Header from "../components/header";
 
-const Success = props => (
-  <Layout>
-
-
-    <div id="main" className="alt">
-      <section id="one">
-        <div className="inner">
-          <header className="major">
-            <h1>Success/Thank You Page</h1>
-          </header>
-          <span className="image main">
-            <img  alt="" />
-          </span>
-          <p>Thank you for contacting us!</p>
+function Success() {
+  return (
+    <>
+    <Header/>
+    <Layout>
+      {/* <SEO
+        keywords={[`gatsby`, `tailwind`, `react`, `tailwindcss`]}
+        title="Contact"
+      /> */}
+      <section className="flex flex-wrap mx-auto items-center w-10/12 lg:w-2/3 md:w-2/3 sm:w-4/5 h-screen">
+        <div className="w-full">
+            <h2 className=" text-5xl underline font-bold text-gray-700">
+                Grazie per avermi contattato, ci sentiamo presto!
+            </h2>
         </div>
+        <div className="w-full">
+             <h2 className="text-5xl underline font-bold text-gray-700">
+                 Grazie per avermi contattato, ci sentiamo presto!
+            </h2>
+        </div>
+            
       </section>
-    </div>
-  </Layout>
-);
+    </Layout>
+    </>
+  );
+}
 
 export default Success;
