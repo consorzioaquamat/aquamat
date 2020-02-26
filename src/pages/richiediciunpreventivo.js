fe6437bb1474ea2e700a1086f8d94255bb1571b7
lcsrbn <luca.sorbini@gmail.com>
2020-02-26 22:04:11 +0100

    initial commit

--- src/pages/richiediciunpreventivo.js
+++ src/pages/richiediciunpreventivo.js
@@ -12,7 +12,14 @@ function RichiediciunpreventivoPage() {
         title="Contact"
       /> */}
       <section className="mb-8 flex m-auto sm:justify-center w-10/12 lg:w-2/3 md:w-2/3 sm:w-4/5">
-        <form className="w-full">
+        <form  name="preventivo-form"
+          method="post"
+          data-netlify="true"
+          data-netlify-honeypot="bot-field" 
+          action="/success"
+          className="w-full">
+          
+          <input type="hidden" name="form-name" value="preventivo-form" />
           <div className="flex flex-wrap ">
    
             <h2 className="mb-4 leading-tight text-3xl font-bold text-yellow-400">
@@ -23,66 +30,64 @@ function RichiediciunpreventivoPage() {
         
           <label
             className="block mb-2 text-xs font-bold uppercase"
-            htmlFor="first-name"
+
           >
             Info
           </label>
           <input
             className="w-full mb-6 form-input"
-            id="first-name"
+            name="first-name"
             placeholder="Nome Azienda"
             type="text"
           />
 
           <label
             className="block mb-2 text-xs font-bold uppercase"
-            htmlFor="indirizzo-email"
           >
             Indirizzo Email
           </label>
           <input
             className="w-full mb-6 form-input"
-            id="indirizzo-email"
+            name="indirizzo-email"
             placeholder="Indirizzo Email"
             type="text"
           />
 
           <label
             className="block mb-2 text-xs font-bold uppercase"
-            htmlFor="Contact-number"
+
           >
             Contatto Telefonico
           </label>
           <input
             className="w-full mb-6 form-input"
-            id="first-name"
+            name="first-name"
             placeholder="Num. Telefono"
             type="text"
           />
 
           <label
             className="block mb-2 text-xs font-bold uppercase"
-            htmlFor="Contact-number"
           >
             Regione/Provincia
           </label>
           <input
             className="w-full mb-6 form-input"
-            id="regione-provincia"
+            name="regione-provincia"
             placeholder="Indicare regione e città dell'azienda e/o Cantiere"
             type="text"
           />
 
           <label
             className="block mb-2 text-xs font-bold uppercase"
-            htmlFor="message"
+
           >
             Messaggio
           </label>
 
           <textarea
             className="w-full mb-6 form-textarea"
-            id="message"
+            name="message"
             placeholder="Inserisci i dettagli della richiesta..."
             rows="8"
           />
@@ -91,7 +96,7 @@ function RichiediciunpreventivoPage() {
           <div class="md:flex md:items-center mb-6">
           
               <label class="md:w-2/3 block text-gray-500 font-bold">
-                <input class="mr-2 leading-tight" type="checkbox"/>
+                <input class="mr-2 leading-tight"  name=" Do il consenso al trattamento dei dati forniti" type="checkbox"/>
                 <span class="text-xs font-thin leading-tight ">
                 Do il consenso al trattamento dei dati forniti <br/> <a class="text-small text-gray-500" href="">Informativa ai sensi del D.Lgs. 196/2003 "Codice in materia di protezione dei dati personali"</a>
                 </span>
@@ -99,7 +104,7 @@ function RichiediciunpreventivoPage() {
        
           </div>    
 
-          <button className="w-1/4 text-sm md:text-base ml-auto right-0 h-12 my-auto bg-yellow-400 hover:bg-transparent text-black hover:text-gray-700 font-semibold py-2 px-2  sm:px-4 border border-yellow-400 rounded focus:outline-none ">
+          <button type="submit" className="w-1/4 text-sm md:text-base ml-auto right-0 h-12 my-auto bg-yellow-400 hover:bg-transparent text-black hover:text-gray-700 font-semibold py-2 px-2  sm:px-4 border border-yellow-400 rounded focus:outline-none ">
              Invia
           </button>
         </form>
--- src/pages/uniscitianoi.js
+++ src/pages/uniscitianoi.js
@@ -19,7 +19,7 @@ function UniscitianoiPage() {
          data-netlify-honeypot="bot-field" 
          action="/success"
          className="w-full">
-           <input type="hidden" name="form-name" value="join-us-form" />
+          <input type="hidden" name="form-name" value="join-us-form" />
           <div className="flex flex-wrap ">
    
             <h2 className="mb-4 leading-tight text-3xl font-bold text-yellow-400">
@@ -29,9 +29,9 @@ function UniscitianoiPage() {
           
           <label className="block text-xs mb-2 font-bold uppercase">Sono iteressato a diventare</label>
           <div class="inline-block relative w-64 mb-4">
-            <select class="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
-              <option name="Associato">Un vostro associato</option>
-              <option name="Fornitore">Un vostro fornitore</option>
+            <select  name="candidatura" class="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
+              <option value="Associato">Un vostro associato</option>
+              <option value="Fornitore">Un vostro fornitore</option>
             </select>
             <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
               <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
