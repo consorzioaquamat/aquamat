dc18116a51a775f9a972c39a266d4db85e7df158
lcsrbn <luca.sorbini@gmail.com>
2020-02-26 21:51:07 +0100

    initial commit

--- src/pages/uniscitianoi.js
+++ src/pages/uniscitianoi.js
@@ -19,6 +19,7 @@ function UniscitianoiPage() {
          data-netlify-honeypot="bot-field" 
          action="/success"
          className="w-full">
+           <input type="hidden" name="form-name" value="join-us-form" />
           <div className="flex flex-wrap ">
    
             <h2 className="mb-4 leading-tight text-3xl font-bold text-yellow-400">
@@ -29,8 +30,8 @@ function UniscitianoiPage() {
           <label className="block text-xs mb-2 font-bold uppercase">Sono iteressato a diventare</label>
           <div class="inline-block relative w-64 mb-4">
             <select class="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
-              <option value="Associato">Un vostro associato</option>
-              <option value="Fornitore">Un vostro fornitore</option>
+              <option name="Associato">Un vostro associato</option>
+              <option name="Fornitore">Un vostro fornitore</option>
             </select>
             <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
               <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
@@ -40,20 +41,20 @@ function UniscitianoiPage() {
         
           <label
             className="block mb-2 text-xs font-bold uppercase"
-            name="first-name"
+           
           >
             Info
           </label>
           <input
             className="w-full mb-6 form-input"
-            id="first-name"
+            name="first-name"
             placeholder="Nome Azienda"
             type="text"
           />
 
           <label
             className="block mb-2 text-xs font-bold uppercase"
-            name="indirizzo-email"
+           
           >
             Indirizzo Email
           </label>
@@ -66,13 +67,13 @@ function UniscitianoiPage() {
 
           <label
             className="block mb-2 text-xs font-bold uppercase"
-            name="Contact-number"
+       
           >
             Contatto Telefonico
           </label>
           <input
             className="w-full mb-6 form-input"
-            named="contatto-telefonico"
+            name="contatto-telefonico"
             placeholder="Num. Telefono"
             type="text"
           />
@@ -80,7 +81,7 @@ function UniscitianoiPage() {
           <div class="md:flex md:items-center mb-6">
           
               <label class="md:w-2/3 block text-gray-500 font-bold">
-                <input class="mr-2 leading-tight"  value=" Do il consenso al trattamento dei dati forniti" type="checkbox"/>
+                <input class="mr-2 leading-tight"  name=" Do il consenso al trattamento dei dati forniti" type="checkbox"/>
                 <span class="text-xs font-thin leading-tight ">
                 Do il consenso al trattamento dei dati forniti <br/> <a class="text-small text-gray-500" href="">Informativa ai sensi del D.Lgs. 196/2003 "Codice in materia di protezione dei dati personali"</a>
                 </span>
