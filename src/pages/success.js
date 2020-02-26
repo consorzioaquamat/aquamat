5dc0a9c56b2e3cbe7102377ad155c30eb058d3ab
lcsrbn <luca.sorbini@gmail.com>
2020-02-26 20:52:51 +0100

    initial commit

---
+++ src/pages/success.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Layout from '../components/layout';
+
+const Success = props => (
+  <Layout>
+
+
+    <div id="main" className="alt">
+      <section id="one">
+        <div className="inner">
+          <header className="major">
+            <h1>Success/Thank You Page</h1>
+          </header>
+          <span className="image main">
+            <img  alt="" />
+          </span>
+          <p>Thank you for contacting us!</p>
+        </div>
+      </section>
+    </div>
+  </Layout>
+);
+
+export default Success;
